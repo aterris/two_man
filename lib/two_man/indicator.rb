333740bb08ab0572c229adfe567d63ff6d149a55
Andrew Terris <atterris@gmail.com>
2014-01-22 23:13:24 -0500

    simpler indicator

--- lib/two_man/indicator.rb
+++ lib/two_man/indicator.rb
@@ -2,38 +2,22 @@ module TwoMan
   class Indicator
     attr_accessor :pins
 
-    def initialize(initial_state = :ready)
-      @pins = {
-        :armed => [PiPiper::Pin.new(:pin => 23, :direction => :out)],
-        :launch => [PiPiper::Pin.new(:pin => 18, :direction => :out)],
-        :error => [PiPiper::Pin.new(:pin => 25, :direction => :out)]
-      }
+    def initialize(initial_state = :off)
+      @pins = [
+        PiPiper::Pin.new(:pin => 18, :direction => :out),
+        PiPiper::Pin.new(:pin => 23, :direction => :out),
+        PiPiper::Pin.new(:pin => 25, :direction => :out)
+      ]
 
       self.send(initial_state)
     end
 
-    def ready
-      @pins[:armed].map(&:off)
-      @pins[:launching].map(&:off)
-      @pins[:error].map(&:off)
+    def off
+      @pins.map(&:off)
     end
 
-    def armed
-      @pins[:armed].map(&:on)
-      @pins[:launching].map(&:off)
-      @pins[:error].map(&:off)
-    end
-
-    def launch
-      @pins[:armed].map(&:off)
-      @pins[:launching].map(&:on)
-      @pins[:error].map(&:off)
-    end
-
-    def error
-      @pins[:armed].map(&:off)
-      @pins[:launching].map(&:off)
-      @pins[:error].map(&:on)
+    def on
+      @pins.map(&:on)
     end
 
   end

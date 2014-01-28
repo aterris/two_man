bfcbc3d4e3e69b3e90bb2a113754631baa0fdb3d
Andrew Terris <atterris@gmail.com>
2014-01-28 01:47:00 -0500

    added first spec

--- spec/two_man/command_spec.rb
+++ spec/two_man/command_spec.rb
@@ -1,1 +1,27 @@
 require 'spec_helper'
+
+describe TwoMan::Command do
+
+  before do
+  end
+
+  it "can normalize repo urls" do
+    repo_urls = [
+      'aterris/launch_codes',
+      'git@github.com:aterris/launch_codes.git',
+      'https://github.com/aterris/launch_codes.git'
+    ]
+
+    repo_urls.each do |repo_url|
+      repo = TwoMan::Command.send(:normalize_repo_url, repo_url)
+
+      repo[:name].should == 'aterris/launch_codes'
+      repo[:path].should include('lib/launch_code/aterris/launch_codes')
+      
+      url = (repo_url.include? '://github.com/') ? 'https://github.com/aterris/launch_codes.git' : 'git@github.com:aterris/launch_codes.git'
+      repo[:url].should == url
+    end
+
+  end
+
+end

12658f16e9076b203dcf276a1dd708a921feccd9
Andrew Terris <atterris@gmail.com>
2014-01-29 19:58:45 -0500

    remove warnings by only defining if not defined

--- lib/two_man.rb
+++ lib/two_man.rb
@@ -3,7 +3,7 @@ lib_path = File.expand_path('../', __FILE__)
 ($:.unshift lib_path) unless ($:.include? lib_path)
 
 # gems
-#require 'pi_piper'
+require 'pi_piper'
 require 'active_support/inflector'
 require 'fileutils'
 require 'git'
